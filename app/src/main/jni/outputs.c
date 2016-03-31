7ceedff04ed68d645b3c8229b47878e3f839dec5
Albert (on sensibility-buildbox-32) <albert.rafetseder+github@univie.ac.at>
2016-03-31 11:00:06 +0200

    Comment out verbose debug logging in `outputs.c`

--- app/src/main/jni/outputs.c
+++ app/src/main/jni/outputs.c
@@ -15,26 +15,25 @@ PyObject* androidlog_log2(PyObject *self, PyObject *python_message)
   jclass output_service_class;
   jmethodID log_message;
 
-  LOGI("Use the cached JVM pointer to get a new environment");
+  // Use the cached JVM pointer to get a new environment
   (*cached_vm)->AttachCurrentThread(cached_vm, &jni_env, NULL);
   
-  LOGI("Find Java class");
+  // Find Java class
   output_service_class = (*jni_env)->FindClass(jni_env, "com/snakei/OutputService");
 
-  LOGI("Find the Java method we're gonna call");
+  // Find the Java method we want to call
   log_message = (*jni_env)->GetStaticMethodID(jni_env, 
       output_service_class, "logMessage", "(Ljava/lang/String;)V");
 
-  LOGI("Convert to C string");
+  // Convert Python string to C string
   c_message = PyString_AsString(python_message);
-  LOGI("Convert to Java string with env %p", jni_env);
+  // Convert C string to Java string
   java_message = (*jni_env)->NewStringUTF(jni_env, c_message);
 
-  LOGI("Call output method");
+  // Call output method
   (*jni_env)->CallStaticVoidMethod(jni_env, output_service_class, 
       log_message, java_message);
 
-  LOGI("I'm done!");
   Py_RETURN_NONE;  // I.e., `return Py_None;` with reference counting
 }
 
