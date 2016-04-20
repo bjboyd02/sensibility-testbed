88036585b3a74e33b787bf3cb6ff21328875f625
Lukas P <luk.puehringer@gmail.com>
2016-04-20 14:44:49 -0400

    adds c-python sensor extension comments

--- app/src/main/jni/sensors.c
+++ app/src/main/jni/sensors.c
@@ -1,2 +1,59 @@
 #include "sensors.h"
 
+/*
+ *
+ * Python extension to get a list of sensor info
+ * for each available sensor.
+ *
+ * Returns
+ *  [{"name" : <name>, "vendor":<vendor>},..]
+ *
+ * This is easy,
+ * - get the cached JVM
+ * - (new thread?)
+ * - execute Java method in JVM using JNI, Java method
+ *  = instantiates a SensorManager
+ *  = calls appropriate getSensor method
+ *  = transforms to a list of dicts of simple data types (char, int, float,..)
+ *  = (destroys the manager?)
+ *  = returns the list
+ * - transform the data to Python
+ * - free memory
+ * - return
+ */
+PyObject* sensor_get_sensor_list(PyObject *self) {
+    
+
+}
+
+/*
+ * Python Extension(s) to get actual sensor values
+ *  e.g. Accelerometer
+ *
+ * This is harder and/as there are multiple approaches,
+ * - Do we want one function for all sensors, like in current Repy Sensor API? [1]
+ * - Do we want to poll a sensor? (let's start with this)
+ *  e.g.:
+ *      start_sensing(TYPE_ACCELEROMETER)
+ *      get_current_value()
+ *      stop_sensing()
+ * - Do we want to use callback functions? (cool but hard)
+ *  e.g.:
+ *       start_sensing(TYPE_ACCELEROMETER, fn_called_with_sensor_values)
+ *       stop_sensing()
+ * - Should we go OO, like Yocto Python API? [2] (not really pythonic, is it?)
+ *  e.g.:
+ *       sensor = find_sensor(TYPE_ACCELEROMETER)
+ *       sensor.start()
+ *       sensor.getValue()
+ *       sensor.stop()
+ *
+ * In any case we need to think of,
+ *  - properly creating, starting, stopping, destroying sensors and sensor listener.
+ *  - multiple sensors for one type.
+ *  - multiple vessels concurrently accessing a sensor.
+ *
+ * [1] https://sensibilitytestbed.com/projects/project/wiki/sensors#Sensors
+ * [2] http://www.yoctopuce.com/EN/products/yocto-meteo/doc/METEOMK1.usermanual.html#CHAP14
+ *
+ */
--- app/src/main/jni/sensors.h
+++ app/src/main/jni/sensors.h
@@ -5,5 +5,7 @@
 #include <jni.h>
 #include "snakei.h"
 
+PyObject* sensor_get_sensor_list(PyObject *self);
+
 #endif /* defined _SNAKEI_SENSORS_H_ */
 
