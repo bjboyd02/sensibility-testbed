a830efc8ad9568050f1fa3d4a8c3eccc7a91ddbc
Lukas P <luk.puehringer@gmail.com>
2016-06-03 16:49:37 -0400

    fixes csv header bug in locations test

--- app/src/main/assets/test_location_file.py
+++ app/src/main/assets/test_location_file.py
@@ -1,12 +1,12 @@
 import androidlog, location, sys, time
 l = androidlog.log2
 
-path = "/storage/emulated/legacy/Android/data/com.sensibility_testbed/files/"
+path = "/sdcard/locations/"
 filename = "locations_" + str(time.time())
 
 with open(path + filename, "w+") as out:
     l('Lets do the location')
-    out.write("time_polled, time_sample, accuracy, altitude, bearing, latitude, longitude, speed\n")
+    out.write("provider, time_polled, time_sample, accuracy, altitude, bearing, latitude, longitude, speed\n")
     while True:
         data = location.get_location()
         l(repr(data))
@@ -16,9 +16,7 @@ with open(path + filename, "w+") as out:
                 out.write("%s,%f,%f,%f,%f,%f,%f,%f,%f\n" % (provider, loc.get("time_polled", -1), loc.get("time_sample", -1), loc.get("accuracy", -1),
                   loc.get("altitude", -1), loc.get("bearing", -1), loc.get("latitude", -1), loc.get("longitude", -1), loc.get("speed", -1)))
 
-
-        
-        time.sleep(0.5)
+        time.sleep(1)
         out.flush()
 
 l('KTHXBI')
