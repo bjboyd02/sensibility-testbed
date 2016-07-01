fe2ad32dc0e4ae717b6a79d27b184a00708a78cb
Lukas P <luk.puehringer@gmail.com>
2016-07-01 16:39:01 -0400

    Deletes obsolote code from snakei and adds comment on caching

--- app/src/main/jni/snakei.c
+++ app/src/main/jni/snakei.c
@@ -1,80 +1,16 @@
 #include "snakei.h"
 
-/* XXX UNUSED!!!
- * Define an array of JNINativeMethod structures to map method names 
- * (as later usable from Java code) and signatures (argument types, 
- * return type) onto C function names.
- *
- * See https://docs.oracle.com/javase/7/docs/technotes/guides/jni/spec/functions.html#wp17734
- */
-static JNINativeMethod methods[] = {
-  // XXX UNUSED!!!
-  //{"startNativePythonInterpreter", "(Ljava/lang/String;Ljava/lang/String;)V", (void*)&start_native_python_interpreter},
-};
-
-
-
-/* XXX UNUSED!!! */
-int cache_and_register_stuff(JavaVM* vm) {
-  jclass python_interpreter_service_class;
-
-  /* Attach the current thread to the Java VM so we can 
-   * get the environment for looking up classes. */
-  LOGI("Attaching current thread");
-  // XXX (*vm)->AttachCurrentThread(vm, &jni_environment, NULL);
-
-  // PythonInterpreterService is where we register our C functions
-  // XXX python_interpreter_service_class = (*jni_environment)->FindClass(jni_environment, "com/snakei/PythonInterpreterService");
-
-  LOGI("Registering natives");
-  /* Register the C code's native functions (so as to save us some 
-   * name mangling)
-   * see https://stackoverflow.com/questions/1010645/what-does-the-registernatives-method-do */
-  /*
-  if ((*jni_environment)->RegisterNatives(jni_environment, python_interpreter_service_class, methods, 
-      sizeof(methods)/sizeof(methods[0])) < 0) {
-    LOGI("Well, that sort of failed.");
-    return -2;
-  }
-  */
-  // From OutputService, we get methods to log/toast/notify
-  // XXX output_service_class = (*jni_environment)->FindClass(jni_environment, "com/snakei/OutputService");
-  /* XXX log_message = (*jni_environment)->GetMethodID(jni_environment, output_service_class, 
-      "logMessage", "(Ljava/lang/String;)V");
-  */
-
-  // TODO From SensorService, we get methods to read sensor values
-
-  // That's all!
-  return 0;
-}
-
-
-
-
-void get_environment(JavaVM* vm) {
-  /* Attach the current thread to the Java VM so we can 
-   * get the environment for looking up classes. */
-  LOGI("Attaching current thread");
-  // XXX (*vm)->AttachCurrentThread(vm, &jni_environment, NULL);
-  LOGI("Done, environment initted");
-}
-
-
 /*
  * This function is called when the Java code does `System.loadLibrary()`.
  * See https://docs.oracle.com/javase/7/docs/technotes/guides/jni/spec/invocation.html#JNI_OnLoad
-*/
+ *
+ * We use it to cache the native reference to the JVM which will be
+ * used by all snakei extensions
+ */
 jint JNI_OnLoad(JavaVM *vm, void *reserved) {
-  cached_vm = vm;
-
-  /*
-  if (cache_and_register_stuff(vm) != 0) {
-    return JNI_ERR;
-  } else {
-  */
+    cached_vm = vm;
     return JNI_VERSION_1_6;
-  //}
+
 }
 
 
--- app/src/main/jni/snakei.h
+++ app/src/main/jni/snakei.h
@@ -19,19 +19,24 @@
 // (taken from the Android NDK samples)
 # define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, __FILE__, __VA_ARGS__))
 
-
-
-/* Following the suggestions in https://developer.android.com/training/articles/perf-jni.html#native_libraries,
+/* Following the suggestions in
+ * https://developer.android.com/training/articles/perf-jni.html#native_libraries,
  * we pre-cache the `JNIEnv` and classes that we will use in C code.
  * First, declare a few references we will populate soon and then reuse 
  * in the course of our runtime.
+ *
+ * XXX:
+ *   we need to cache the JNIEnv per thread, i.e. we shouldn't cache
+ *   it here, because this gets called once by the parent thread that will
+ *   spawn off other threads
+ *
+ *   The vm pointer on the other hand can be cached shared by all threads
+ *   and therefor cached here
+ *
+ *   Java classes and methods are currently cached in the each extension module
+ *   maybe this can be done here
  */
 JavaVM* cached_vm;
-JNIEnv* cached_jni_env;
-jclass cached_output_service_class;
-jmethodID cached_log_message;
-
-
 
 #endif /* defined _SNAKEI_H_ */
 
