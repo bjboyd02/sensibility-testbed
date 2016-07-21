c66418c8e613bf0f4fd73f6fc18575778253e4f8
Lukas P <luk.puehringer@gmail.com>
2016-07-21 09:46:42 -0400

    Adds process test script

---
+++ app/raw/scripts/test_process.py
@@ -0,0 +1,8 @@
+import time
+#import androidlog, os, time
+#l = androidlog.log
+
+
+for x in range(10):
+    #l("PID: " + str(os.getpid()) + " PPID: " + str(os.getppid()) + " UID: " + str(os.getuid()))
+    time.sleep(2)
